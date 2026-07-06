98011f033a87876e3b923a9f8cf70457fdb668a3
LogicInGeneral2 <ajmaj578@gmail.com>
2026-07-06 12:16:33 +0800

    Phase 15: Updated Terraform to include helm, strimzi, elasticsearch, prometheus and grafana

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,19 +4,68 @@ terraform {
       source  = "tehcyx/kind"
       version = "~> 0.4.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.1"
+    }
   }
 }
 
-provider "kind" {}
-
-# Provision a local Kubernetes cluster
+# 1. Create the Local Kind Cluster
 resource "kind_cluster" "job_board_cluster" {
   name           = "job-board-cluster"
   node_image     = "kindest/node:v1.30.0"
   wait_for_ready = true
 }
 
-output "kubeconfig" {
-  value       = kind_cluster.job_board_cluster.kubeconfig
-  description = "The kubeconfig for the local cluster"
+# 2. Configure the Helm Provider to talk to the new cluster
+provider "helm" {
+  kubernetes {
+    host                   = kind_cluster.job_board_cluster.endpoint
+    client_certificate     = kind_cluster.job_board_cluster.client_certificate
+    client_key             = kind_cluster.job_board_cluster.client_key
+    cluster_ca_certificate = kind_cluster.job_board_cluster.cluster_ca_certificate
+  }
+}
+
+# 3. Deploy Strimzi Operator via Helm
+resource "helm_release" "strimzi_operator" {
+  name             = "my-strimzi"
+  repository       = "https://strimzi.io/charts/"
+  chart            = "strimzi-kafka-operator"
+  namespace        = "data-pipeline"
+  create_namespace = true
+
+  depends_on = [kind_cluster.job_board_cluster]
+}
+
+# 4. Deploy Elasticsearch via Helm
+resource "helm_release" "elasticsearch" {
+  name             = "my-elastic"
+  repository       = "https://helm.elastic.co"
+  chart            = "elasticsearch"
+  namespace        = "data-pipeline"
+  create_namespace = true
+
+  values = [
+    file("../../YAML/elastic-values.yaml")
+  ]
+
+  depends_on = [kind_cluster.job_board_cluster]
+}
+
+# 5. Deploy Prometheus & Grafana via Helm
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  set {
+    name  = "grafana.adminPassword"
+    value = "admin"
+  }
+
+  depends_on = [kind_cluster.job_board_cluster]
 }
